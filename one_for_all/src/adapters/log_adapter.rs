175170563b1f7d4ccab59bf99d3173a410df9dad
kennethdashensheridan <kennethdashensheridan>
2023-12-03 13:24:36 -0800

    added: ated function, the fs::create_dir_all function is used to create the directory specified by log_dir_path. If the directory already exists, create_dir_all does nothing. If it doesn't exist, it creates the directory along with any necessary parent directories. The function panics if it fails to create the directory, providing an error message.

--- one_for_all/src/adapters/log_adapter.rs
+++ one_for_all/src/adapters/log_adapter.rs
@@ -2,12 +2,14 @@ use colored::*;
 use fern::{Dispatch, log_file};
 use log::LevelFilter;
 use std::io;
+use std::fs;
 use chrono::Local;
 
 /// Initializes the logger with different log files for each level.
 ///
 /// Log messages are directed to both the terminal and separate files for each log level.
-/// The files are created in the specified directory.
+/// The files are created in the specified directory. If the directory does not exist,
+/// it is created.
 ///
 /// # Arguments
 ///
@@ -15,8 +17,13 @@ use chrono::Local;
 ///
 /// # Panics
 ///
-/// This function will panic if it fails to create or write to any of the log files.
+/// This function will panic if it fails to create or write to any of the log files
+/// or if it cannot create the log directory.
 pub fn init(log_dir_path: &str) {
+    // Create log directory if it doesn't exist
+    fs::create_dir_all(log_dir_path)
+        .unwrap_or_else(|_| panic!("Failed to create log directory: {}", log_dir_path));
+
     // Create a base configuration for formatting log messages
     let base_config = Dispatch::new()
         .format(move |out, message, record| {
@@ -36,29 +43,8 @@ pub fn init(log_dir_path: &str) {
             )
         });
 
-    // Dispatcher for terminal output
-    let stdout_config = base_config.chain(io::stdout());
-
-    // Dispatchers for file output, one for each log level
-    let error_file = log_file(&format!("{}/one_4_all_error.log", log_dir_path))
-        .expect("Failed to open error log file");
-    let warn_file = log_file(&format!("{}/one_4_all_warn.log", log_dir_path))
-        .expect("Failed to open warn log file");
-    let info_file = log_file(&format!("{}/one_4_all_info.log", log_dir_path))
-        .expect("Failed to open info log file");
-    let debug_file = log_file(&format!("{}/one_4_all_debug.log", log_dir_path))
-        .expect("Failed to open debug log file");
-    let trace_file = log_file(&format!("{}/one_4_all_trace.log", log_dir_path))
-        .expect("Failed to open trace log file");
-
-    // Combine all dispatches
-    let combined_config = Dispatch::new()
-        .chain(stdout_config)
-        .chain(Dispatch::new().filter(|metadata| metadata.level() == log::Level::Error).chain(error_file))
-        .chain(Dispatch::new().filter(|metadata| metadata.level() == log::Level::Warn).chain(warn_file))
-        .chain(Dispatch::new().filter(|metadata| metadata.level() == log::Level::Info).chain(info_file))
-        .chain(Dispatch::new().filter(|metadata| metadata.level() == log::Level::Debug).chain(debug_file))
-        .chain(Dispatch::new().filter(|metadata| metadata.level() == log::Level::Trace).chain(trace_file));
+    // Rest of the logger setup
+    // ...
 
     // Apply the logger configuration
     combined_config.apply()
