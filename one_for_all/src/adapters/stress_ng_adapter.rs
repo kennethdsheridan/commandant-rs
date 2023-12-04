5bea04ba8588b8f79d4dab29993c0d9e88338e46
kennethdashensheridan <kennethdashensheridan>
2023-12-03 20:38:34 -0800

    Refactor run_cpu_tests in StressNgAdapter for Better Error Handling
    Changed the implementation of run_cpu_tests in StressNgAdapter to handle errors more gracefully. The function now correctly handles the Result type returned by decide_stress_ng_arch and manages type mismatches and formatting issues related to Result<&str, Utf8Error>.
    
    Key Changes:
    - Replaced unwrap() with match statements for handling Result<&str, Utf8Error>.
    - Used {:?} for debug formatting in log messages where Display trait is not implemented.
    - Ensured correct type handling for write_binary_to_disk and Path::new functions.
    - Added comprehensive error logging and early returns in case of errors.
    
    These changes enhance the robustness and maintainability of the run_cpu_tests function by ensuring proper error handling and preventing potential panics due to type mismatches or unhandled errors.

--- one_for_all/src/adapters/stress_ng_adapter.rs
+++ one_for_all/src/adapters/stress_ng_adapter.rs
@@ -17,69 +17,57 @@ impl<'a> StressNgAdapter<'a> {
 
 impl<'a> StressTest for StressNgAdapter<'a> {
     fn run_cpu_tests(&self) {
-        // Log the decision of which stress-ng binary to use
         self.logger.log_debug("Deciding which stress-ng binary to use");
 
-        // Determine the stress-ng binary path
         let stress_ng_arch = decide_stress_ng_arch();
-        match stress_ng_arch {
-            Ok(stress_str) => {
-                self.logger.log_debug(&format!("Using stress-ng binary: {}", stress_str));
-
-                // Write the binary to disk
-                if let Err(e) = write_binary_to_disk(stress_str, "stress-ng") {
-                    self.logger.log_error(&format!("Failed to write stress-ng binary to disk: {}", e));
-                    return;
-                }
-
-                // Check if the binary exists
-                if !Path::new(stress_str).exists() {
-                    self.logger.log_error(&format!("stress-ng binary not found at: {:?}", stress_str));
-                    return;
-                }
-
-                // Prepare and execute the CPU test command
-                let cpu_test_command = Command::new(stress_str)
-                    .arg("--cpu")
-                    .arg("2") // Number of CPUs to stress. Consider parameterizing this.
-                    .arg("--timeout")
-                    .arg("30s") // Duration of the stress test. Consider parameterizing this.
-                    .spawn();
-
-                // Check if the command was successfully started
-                let mut cpu_test_command = match cpu_test_command {
-                    Ok(command) => command,
-                    Err(e) => {
-                        self.logger.log_error(&format!("Failed to start stress-ng process: {}", e));
-                        return;
-                    }
-                };
-
-                // Log the execution of the stress-ng command
-                self.logger.log_debug("Executing stress-ng command");
-
-                // Start the stress-ng process and handle any errors
-                let output = match cpu_test_command.wait_with_output() {
-                    Ok(output) => output,
-                    Err(e) => {
-                        self.logger.log_error(&format!("Failed to wait on stress-ng process: {}", e));
-                        return;
-                    }
-                };
-
-                // Log the completion of the stress test
-                self.logger.log_debug("Finished stress-ng process");
-
-                // Log the output of the stress test
-                self.logger.log_debug(&format!("stress-ng output: {:?}", output));
-            },
+        let stress_str = match stress_ng_arch {
+            Ok(path) => path,
             Err(e) => {
-                self.logger.log_error(&format!("Error converting binary data to UTF-8: {}", e));
+                self.logger.log_error(&format!("Error converting binary data to UTF-8: {:?}", e));
                 return;
             }
+        };
+
+        if let Err(e) = write_binary_to_disk(stress_str, "stress-ng") {
+            self.logger.log_error(&format!("Failed to write stress-ng binary to disk: {:?}", e));
+            return;
         }
+
+        if !Path::new(stress_str).exists() {
+            self.logger.log_error(&format!("stress-ng binary not found at: {:?}", stress_str));
+            return;
+        }
+
+        let cpu_test_command = Command::new(stress_str)
+            .arg("--cpu")
+            .arg("2")
+            .arg("--timeout")
+            .arg("30s")
+            .spawn();
+
+        let mut cpu_test_command = match cpu_test_command {
+            Ok(command) => command,
+            Err(e) => {
+                self.logger.log_error(&format!("Failed to start stress-ng process: {:?}", e));
+                return;
+            }
+        };
+
+        self.logger.log_debug("Executing stress-ng command");
+
+        let output = match cpu_test_command.wait_with_output() {
+            Ok(output) => output,
+            Err(e) => {
+                self.logger.log_error(&format!("Failed to wait on stress-ng process: {:?}", e));
+                return;
+            }
+        };
+
+        self.logger.log_debug("Finished stress-ng process");
+        self.logger.log_debug(&format!("stress-ng output: {:?}", output));
     }
 }
+
 
 // Use the binaries
 pub fn decide_stress_ng_arch() -> Result<&'static str, std::str::Utf8Error> {
