1973416b87471e1a6f0b0af65b2fb0dd206e9f69
kennethdashensheridan <kennethdashensheridan>
2023-12-07 20:14:37 -0800

    added: documentation & comments

--- one_for_all/src/ports/stress_test.rs
+++ one_for_all/src/ports/stress_test.rs
@@ -1,5 +1,56 @@
-// StressTest trait is a port in the hexagonal architecture.
+/// `StressTest` Trait
+///
+/// This trait is a port in the hexagonal (or ports-and-adapters) architecture.
+/// It abstracts the functionality for running stress tests on a system.
+/// Implementors of this trait can provide different strategies for performing stress tests,
+/// such as CPU load, memory pressure, I/O stress, etc.
+///
+/// The `StressTest` trait acts as an interface that defines how stress tests should be executed,
+/// allowing for different implementations that can be swapped out without affecting the core logic
+/// of the application. This approach follows the Dependency Inversion Principle, promoting
+/// loose coupling and high flexibility in the application design.
+///
+/// ## Methods
+///
+/// - `run_cpu_tests`: Method to execute CPU stress tests. Implementations of this method
+///   should contain the logic to stress the CPU resources of the system.
+///
+/// ## Extensions
+///
+/// Additional methods for different types of stress tests (like memory, I/O, etc.)
+/// can be added to this trait. This allows for a unified interface for all stress testing,
+/// making it easy to extend and maintain.
+///
+/// ## Examples
+///
+/// Implementing the `StressTest` trait for a specific stress testing strategy:
+///
+/// ```rust
+/// struct MyStressTester;
+///
+/// impl StressTest for MyStressTester {
+///     fn run_cpu_tests(&self) {
+///         // Implementation for CPU stress testing
+///     }
+/// }
+///
+/// let tester = MyStressTester;
+/// tester.run_cpu_tests(); // Execute the CPU stress tests
+/// ```
 pub trait StressTest {
+    /// Executes CPU stress tests.
+    ///
+    /// This method should be implemented to define how the CPU stress tests are conducted.
+    /// The implementation can vary based on the specific requirements or characteristics
+    /// of the stress testing tool or strategy being used.
+    ///
+    /// Implementors should ensure that this method effectively stresses the CPU
+    /// and can optionally provide mechanisms for monitoring or logging the test progress
+    /// and results.
     fn run_cpu_tests(&self);
-    // Other stress test methods can be added here
+
+    // Placeholder for other stress test methods.
+    // For example:
+    // fn run_memory_tests(&self);
+    // fn run_io_tests(&self);
 }
