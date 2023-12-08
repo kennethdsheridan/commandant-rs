be9a6470abf34bd1152279b057edae25f9ec79fc
kennethdashensheridan <kennethdashensheridan>
2023-12-07 20:02:30 -0800

    added: documentation & comments

--- one_for_all/src/adapters/stress_ng_adapter.rs
+++ one_for_all/src/adapters/stress_ng_adapter.rs
@@ -23,61 +23,78 @@ impl<'a> StressNgAdapter<'a> {
 }
 
 
+/// Implementation of `StressTest` trait for `StressNgAdapter`.
+/// This struct is responsible for running stress tests using the `stress-ng` utility.
 impl<'a> StressTest for StressNgAdapter<'a> {
+    /// Runs CPU stress tests.
+    /// This method decides which `stress-ng` binary to use based on the operating system,
+    /// writes the binary to a temporary file, sets the necessary permissions, and then executes the binary
+    /// with specific arguments to stress test the CPU.
     fn run_cpu_tests(&self) {
+        // Log the start of the decision process for choosing the appropriate binary
         self.logger.log_debug("Deciding which stress-ng binary to use");
 
+        // Choose the appropriate binary data based on the target OS
         let binary_data = if cfg!(target_os = "linux") {
             STRESS_NG_LINUX
         } else {
             STRESS_NG_MACOS
         };
 
-        // Generate a unique temporary file name
+        // Define a path for the temporary file where the binary will be written
         let temp_file_path = format!("/tmp/{}", "stress-ng");
-        // Write the binary data to a file
+
+        // Attempt to create the file and write the binary data to it
+        // If there is an error during this process, log the error and return early
         if let Err(e) = File::create(&temp_file_path)
             .and_then(|mut file| file.write_all(binary_data)) {
             self.logger.log_error(&format!("Failed to write stress-ng binary to disk: {:?}", e));
             return;
         }
 
-        // Set executable permissions
+        // Set the file permissions to make the binary executable
+        // Permissions are set to 755 (read, write, and execute for owner; read and execute for others)
         let mut perms = fs::metadata(&temp_file_path).unwrap().permissions();
-        perms.set_mode(0o755); // Read, write, and execute for owner; read and execute for others
+        perms.set_mode(0o755);
         fs::set_permissions(&temp_file_path, perms).unwrap();
 
-
-        // Prepare and execute the CPU test command
+        // Prepare the command to execute the stress test
+        // Here, the stress test is configured to stress two CPUs for 30 seconds
+        // These parameters could be made configurable
         let cpu_test_command = Command::new(temp_file_path)
             .arg("--cpu")
-            .arg("2") // Number of CPUs to stress, consider parameterizing this
+            .arg("2") // Number of CPUs to stress
             .arg("--timeout")
-            .arg("30s") // Duration of the stress test, consider parameterizing this
+            .arg("30s") // Duration of the stress test
             .spawn();
 
         // Check if the command was successfully started
         match cpu_test_command {
             Ok(command) => {
+                // Log the successful execution of the command
                 self.logger.log_debug("Executing stress-ng command");
 
-                // Start the stress-ng process and handle any errors
+                // Wait for the command to complete and log the output or any errors encountered
                 match command.wait_with_output() {
                     Ok(output) => {
+                        // Log the successful completion and the output of the command
                         self.logger.log_debug("Finished stress-ng process");
                         self.logger.log_debug(&format!("stress-ng output: {:?}", output));
                     },
                     Err(e) => {
+                        // Log any errors encountered while waiting for the command to complete
                         self.logger.log_error(&format!("Failed to wait on stress-ng process: {:?}", e));
                     }
                 }
             },
             Err(e) => {
+                // Log any errors encountered while starting the command
                 self.logger.log_error(&format!("Failed to start stress-ng process: {:?}", e));
             }
         }
     }
 }
+
 
 
 // Use the binaries
