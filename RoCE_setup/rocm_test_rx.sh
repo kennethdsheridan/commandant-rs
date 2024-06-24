1ef173977b8b291bb7b0338b48a60149771bfc34
Kenny (Knight) Sheridan <kennethdashensheridan@gmail.com>
2024-06-23 22:40:06 -0700

    removed iterating the bind addresses

--- RoCE_setup/rocm_test_rx.sh
+++ RoCE_setup/rocm_test_rx.sh
@@ -6,24 +6,30 @@ LOGFILE="$HOME/perftest/logs/ib_write_bw_server.log"
 # Create log directory if it doesn't exist
 mkdir -p $(dirname $LOGFILE)
 
-# List of NIC devices
-NIC_DEVICES=("192.168.42.0" "192.168.42.1" "192.168.42.2" "192.168.42.3" "192.168.42.4" "192.168.42.5" "192.168.42.6" "192.168.42.7")
+# List of RDMA devices
+RDMA_DEVICES=("bnxt_re0" "bnxt_re1" "bnxt_re2" "bnxt_re3" "bnxt_re6" "bnxt_re7" "bnxt_re8" "bnxt_re9")
+
+# List of ROCm devices
+ROCM_DEVICES=(0 1 2 3 4 5 6 7)
 
 # Function to run the command and log the output
 run_server_test() {
-    local nic_device=$1
-    echo "Running ib_write_bw server test at $(date) for NIC $nic_device" | tee -a $LOGFILE
-    $HOME/perftest/bin/ib_write_bw -d bnxt_re0 --use_rocm=1 -a -F -x 3 -q 4 --report_gbits -b --bind_source_ip $nic_device 2>&1 | tee -a $LOGFILE
-    echo "Server test completed at $(date) for NIC $nic_device" | tee -a $LOGFILE
+    local rdma_device=$1
+    local rocm_device=$2
+    echo "Running ib_write_bw server test at $(date) for RDMA device $rdma_device and ROCm device $rocm_device" | tee -a $LOGFILE
+    $HOME/perftest/bin/ib_write_bw -d $rdma_device --use_rocm=$rocm_device -a -F -x 3 -q 4 --report_gbits -b 2>&1 | tee -a $LOGFILE
+    echo "Server test completed at $(date) for RDMA device $rdma_device and ROCm device $rocm_device" | tee -a $LOGFILE
     echo "----------------------------------------" | tee -a $LOGFILE
 }
 
-# Loop to run the test for all NIC devices
+# Loop to run the test for all combinations of RDMA and ROCm devices
 while true; do
-    for nic in "${NIC_DEVICES[@]}"; do
-        run_server_test $nic
-        # Wait time between tests (in seconds)
-        sleep 10
+    for rdma in "${RDMA_DEVICES[@]}"; do
+        for rocm in "${ROCM_DEVICES[@]}"; do
+            run_server_test $rdma $rocm
+            # Wait time between tests (in seconds)
+            sleep 10
+        done
     done
 done
 
--- RoCE_setup/rocm_test_tx.sh
+++ RoCE_setup/rocm_test_tx.sh
@@ -6,27 +6,27 @@ LOGFILE="$HOME/perftest/logs/ib_write_bw_client.log"
 # Create log directory if it doesn't exist
 mkdir -p $(dirname $LOGFILE)
 
-# List of GPU devices
-GPU_DEVICES=("bnxt_re0" "bnxt_re1" "bnxt_re2" "bnxt_re3" "bnxt_re4" "bnxt_re5" "bnxt_re6" "bnxt_re7")
+# List of RDMA devices
+RDMA_DEVICES=("bnxt_re0" "bnxt_re1" "bnxt_re2" "bnxt_re3" "bnxt_re6" "bnxt_re7" "bnxt_re8" "bnxt_re9")
 
-# List of NIC devices
-NIC_DEVICES=("192.168.42.0" "192.168.42.1" "192.168.42.2" "192.168.42.3" "192.168.42.4" "192.168.42.5" "192.168.42.6" "192.168.42.7")
+# List of ROCm devices
+ROCM_DEVICES=(0 1 2 3 4 5 6 7)
 
 # Function to run the command and log the output
 run_client_test() {
-    local gpu_device=$1
-    local nic_device=$2
-    echo "Running ib_write_bw client test at $(date) for GPU $gpu_device and NIC $nic_device" | tee -a $LOGFILE
-    $HOME/perftest/bin/ib_write_bw -d $gpu_device --use_rocm=0 -a -F -x 3 -q 4 --report_gbits -b --bind_source_ip $nic_device 192.168.42.2 2>&1 | tee -a $LOGFILE
-    echo "Client test completed at $(date) for GPU $gpu_device and NIC $nic_device" | tee -a $LOGFILE
+    local rdma_device=$1
+    local rocm_device=$2
+    echo "Running ib_write_bw client test at $(date) for RDMA device $rdma_device and ROCm device $rocm_device" | tee -a $LOGFILE
+    $HOME/perftest/bin/ib_write_bw -d $rdma_device --use_rocm=$rocm_device -a -F -x 3 -q 4 --report_gbits -b --bind_source_ip 192.168.42.3 192.168.42.2 2>&1 | tee -a $LOGFILE
+    echo "Client test completed at $(date) for RDMA device $rdma_device and ROCm device $rocm_device" | tee -a $LOGFILE
     echo "----------------------------------------" | tee -a $LOGFILE
 }
 
-# Loop to run the test for all combinations of GPU and NIC devices
+# Loop to run the test for all combinations of RDMA and ROCm devices
 while true; do
-    for gpu in "${GPU_DEVICES[@]}"; do
-        for nic in "${NIC_DEVICES[@]}"; do
-            run_client_test $gpu $nic
+    for rdma in "${RDMA_DEVICES[@]}"; do
+        for rocm in "${ROCM_DEVICES[@]}"; do
+            run_client_test $rdma $rocm
             # Wait time between tests (in seconds)
             sleep 10
         done
